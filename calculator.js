32f7354008b2e02c6cf921c0b3a5779a36ae2b81
Atia-Khan <atia.khan@xloopdigital.com>
2023-04-16 17:11:52 +0500

    code refactored

--- calculator.js
+++ calculator.js
@@ -8,18 +8,15 @@ btnArray.forEach((button) => {
     if (event.target.innerHTML == "=") {
       string = eval(string);
       input.value = string;
+    } else if (event.target.innerHTML == "AC") {
+      string = "";
+      input.value = string;
+    } else if (event.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += event.target.innerHTML;
+      input.value = string;
     }
-    else if(event.target.innerHTML == 'AC'){
-        string = '';
-        input.value = string;
-    } else if (event.target.innerHTML == 'DEL'){
-        string = string.substring(0,string.length - 1);
-        input.value = string;
-    } else{
-        string += event.target.innerHTML;
-        input.value = string;
-    }
-    
-    
   });
 });
